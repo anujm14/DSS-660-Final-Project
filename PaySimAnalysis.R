79293dc45264fae96328331260bec4e0fd9d0e71
Anuj Mishra <anuj.mishra14@gmail.com>
2017-04-06 00:59:10 -0400

    Data Processing in R
    I have taken problem data set in R and stored in data frame. Please check out few results to understand data set.

---
+++ PaySimAnalysis.R
@@ -0,0 +1,11 @@
+
+# upload data set in R
+
+pysm <- read.csv('D:/MS BIA/Data Mining Project/PaySim Fraud Detection/ProblemDataSet.csv', header = TRUE, sep = ',')
+
+# An overview of data set
+str(pysm)
+
+# Quick summary of data set
+summary(pysm)
+
